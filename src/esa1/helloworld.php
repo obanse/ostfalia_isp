713a420df417034ab287515e56d4137419f81aff
Oliver Banse <o.banse@ostfalia.de>
2019-05-22 15:28:24 +0200

    asdaf

---
+++ src/esa1/helloworld.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by
+ * User: Oliver Banse
+ * Date: 29.04.2019
+ */
+
+echo "<h1>Hello World!</h1>";
--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
-<h1>Internetserverprogrammierung<h1>
+<h1>Internetserverprogrammierung</h1>
 
-<h2>Aufgabe 1<h2>
+<h2>Aufgabe 1</h2>
+
+<a href="esa1/helloworld.php">Zur ESA1 - Hello World!</a>
+
+<h2>Aufgabe 2</h2>
+
+<a href="esa1/helloworld.php">Zur ESA2 - Wunschzettel</a>
